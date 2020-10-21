b7740bb0881cc228d7b06a478ef0cfec06a5f054
Mitchell Zeiter <mzeiter07@gmail.com>
2020-10-21 10:56:02 -0400

    add close/open and backdrop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SidebarComponent } from '@syncfusion/ej2-angular-navigations';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('sidebar') sidebar: SidebarComponent;
+  public showBackdrop: boolean = true;
+  public type: string = 'Push';
+  public width: string ='280px';
+  public closeOnDocumentClick: boolean = true;
+  public onCreated(args: any) {
+    this.sidebar.element.style.visibility = '';
+  }
+  closeClick(): void {
+    this.sidebar.hide();
+  };
+
+  toggleClick():void{
+    this.sidebar.show();
+  }
   title = 'fisher-leadership-app';
 }
