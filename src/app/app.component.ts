fc4d3e1e9c85521ff9469bb80bfabb5a2216475a
Mitchell Zeiter <mzeiter07@gmail.com>
2020-10-22 16:10:26 -0400

    Added collapsible sidebar and top nav-bar (#35)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { SidebarComponent } from '@syncfusion/ej2-angular-navigations';
+import { ButtonComponent } from "@syncfusion/ej2-angular-buttons";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fisher-leadership-app';
+    @ViewChild('sidebar') sidebar: SidebarComponent;
+    @ViewChild('togglebtn') togglebtn: ButtonComponent;
+    public showBackdrop: boolean = true;
+    public width: string ='280px';
+    public closeOnDocumentClick: boolean = true;
+    public type: string = 'Push';
+
+    public onCreated(args: any) {
+         this.sidebar.element.style.visibility = '';
+    }
+    closeClick(): void {
+        this.sidebar.hide();
+    };
+
+    toggleClick():void{
+      this.sidebar.show();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { SidebarModule } from '@syncfusion/ej2-angular-navigations';
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SidebarModule,
+    ButtonModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
