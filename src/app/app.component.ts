8d76a7bfa2bc4763597a3be39f6ee59a1385803f
Mitchell Zeiter <mzeiter07@gmail.com>
2020-11-15 22:36:03 -0500

    highlight dashboard on start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
 
     public onCreated(args: any) {
          this.sidebar.element.style.visibility = '';
+         this.selected = 'Dashboard'
     }
     closeClick(): void {
         this.sidebar.hide();
